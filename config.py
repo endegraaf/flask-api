06d6a1f78579aaea006aff42d3e5e74e6f07626a
Eric de Graaf <endegraaf@gmail.com>
2021-10-03 23:42:44 +0200

    Update config.py

--- config.py
+++ config.py
@@ -1,4 +1,3 @@
-#create table `rest_emp` ( `id` int(11) NOT NULL AUTO_INCREMENT, `name` varchar(255) NOT NULL, `email` varchar(255) NOT NULL,  `phone` varchar(16) DEFAULT NULL, `address` text DEFAULT NULL, PRIMARY KEY(`id`) );
 from app import app
 from flaskext.mysql import MySQL
 
