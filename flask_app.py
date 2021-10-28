777b4c189d6619347c619844e2dd3b6b1a524516
Eric de Graaf <egraaf@thinkpad.local>
2021-10-28 21:47:19 +0200

    Initial commit

---
+++ flask_app.py
@@ -0,0 +1,157 @@
+import pymysql
+from flask import jsonify, render_template
+from flask import request
+from werkzeug.security import check_password_hash
+
+from app import app, auth
+from config import mysql, users
+
+
+@auth.verify_password
+def verify_password(username, password):
+    if username in users and \
+            check_password_hash(users.get(username), password):
+        return username
+
+
+@app.route('/')
+@app.route('/index')
+@auth.login_required
+def index():
+    user = {'username': auth.current_user()}
+    posts = [
+        {
+            'author': {'username': 'C. S. Lewis'},
+            'body': 'The Chronicles of Narnia'
+        },
+        {
+            'author': {'username': 'John'},
+            'body': 'Beautiful day in Portland!'
+        },
+        {
+            'author': {'username': 'Susan'},
+            'body': 'The Avengers movie was so cool!'
+        }
+    ]
+    return render_template('index.html', title='Home', user=user, posts=posts)
+
+
+@app.route('/scrape')
+@auth.login_required
+def scrape():
+    user = {'username': auth.current_user()}
+    datas = [{'vacatures': 'asdf asdf 1234 '}]
+    return render_template('scrape.html', title='Scrape', user=user, posts=datas)
+
+
+@app.route('/add', methods=['POST'])
+@auth.login_required
+def add_emp():
+    try:
+        _json = request.json
+        _name = _json['name']
+        print(_name)
+        _email = _json['email']
+        print(_email)
+        _phone = _json['phone']
+        print(_phone)
+        _address = _json['address']
+        print(_address)
+        if _name and _email and _phone and _address and request.method == 'POST':
+            sqlQuery = "INSERT INTO rest_emp(name, email, phone, address) VALUES(%s, %s, %s, %s)"
+            bindData = (_name, _email, _phone, _address)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+            response = jsonify('Employee added successfully!')
+            response.status_code = 200
+            return response
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/emp')
+@auth.login_required
+def emp():
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor(pymysql.cursors.DictCursor)
+        cursor.execute("SELECT id, name, email, phone, address FROM rest_emp")
+        empRows = cursor.fetchall()
+        respone = jsonify(empRows)
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/update', methods=['PUT'])
+@auth.login_required
+def update_emp():
+    try:
+        _json = request.json
+        _id = _json['id']
+        _name = _json['name']
+        _email = _json['email']
+        _phone = _json['phone']
+        _address = _json['address']
+        if _name and _email and _phone and _address and _id and request.method == 'PUT':
+            sqlQuery = "UPDATE rest_emp SET name=%s, email=%s, phone=%s, address=%s WHERE id=%s"
+            bindData = (_name, _email, _phone, _address, _id,)
+            conn = mysql.connect()
+            cursor = conn.cursor()
+            cursor.execute(sqlQuery, bindData)
+            conn.commit()
+            respone = jsonify('Employee updated successfully!')
+            respone.status_code = 200
+            return respone
+        else:
+            return not_found()
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.route('/delete/<int:id>', methods=['DELETE'])
+@auth.login_required
+def delete_emp(id):
+    try:
+        conn = mysql.connect()
+        cursor = conn.cursor()
+        cursor.execute("DELETE FROM rest_emp WHERE id =%s", (id,))
+        conn.commit()
+        respone = jsonify('Employee deleted successfully!')
+        respone.status_code = 200
+        return respone
+    except Exception as e:
+        print(e)
+    finally:
+        cursor.close()
+        conn.close()
+
+
+@app.errorhandler(404)
+@auth.login_required
+def not_found(error=None):
+    message = {
+        'status': 404,
+        'message': 'Record not found: ' + request.url,
+    }
+    respone = jsonify(message)
+    respone.status_code = 404
+    return respone
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
